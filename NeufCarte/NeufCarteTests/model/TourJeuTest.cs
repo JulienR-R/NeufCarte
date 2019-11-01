79181e797a11bf848bc8ef138f0cfc67ba1b3b75
JulienR-R <>
2019-10-31 20:58:06 -0400

    La Plupart Test model Fait

--- NeufCarte/NeufCarteTests/model/TourJeuTest.cs
+++ NeufCarte/NeufCarteTests/model/TourJeuTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeufCarte.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace NeufCarteTests.model
 {
-    class TourJeuTest
+    [TestClass()]
+    public class TourJeuTest
     {
+        [TestMethod()]
+        public void ResultatTest()
+        {
+            Joueur premierJoueur = new Joueur();
+            Couleur couleurCarte = Couleur.Carreau;
+            Carte cartePlusHaute = new Carte(Valeur.Trois, Couleur.Carreau);
+            Joueur joueurPlusHaut = new Joueur();
+            TourJeu tourJeu = new TourJeu(premierJoueur,
+                                        couleurCarte,
+                                        cartePlusHaute,
+                                        joueurPlusHaut);
+            Assert.AreEqual(premierJoueur, tourJeu.JoueurPlusHaut);
+            Assert.AreEqual(couleurCarte, tourJeu.CouleurCarte);
+            Assert.AreEqual(cartePlusHaute, tourJeu.CartePlusHaute);
+            Assert.AreEqual(joueurPlusHaut, tourJeu.JoueurPlusHaut);
+        }
     }
 }
